46d4029f5f6d32edc318358d078b2d790f186f12
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-02-10 20:42:55 +0000

    Cleanup.

--- src/Controller.js
+++ src/Controller.js
@@ -2,11 +2,8 @@ Controller = function() {
 }
 
 Controller.input = function() {
-  // alert('foo');
-  // console.log($("#input").attr("value"));
   var wallet = new Wallet();
   wallet.pick($("#input").val());
-  // console.log(wallet.set);
 
   var res = "Pick from your wallet: ";
   var max = wallet.set.length;
@@ -18,13 +15,10 @@ Controller.input = function() {
 
   for(var pence in wallet.set.reverse()) {
     var num = wallet.set[pence];
-    // console.log(pence, num);
     if(num > 0)
       res += Wallet.format(max - 1 - pence) + ": " + num;
       if(max - 1 - pence != min) res += ", ";
   }
-
-  // res = "<p>" + res + "</p>";
 
   $("#result").html(res);
 }
